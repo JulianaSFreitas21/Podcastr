b7d0cd8840b00964dcf2ff14b9925418b57f7f16
Juliana dos Santos Freitas <jujugemeasf@gmail.com>
2021-04-23 19:28:48 -0300

    finished

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document{
-  renderI(){
+  render(){
     return (
       <Html>
         <Head>
